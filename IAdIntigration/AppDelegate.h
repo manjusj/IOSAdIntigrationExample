5c69e6983aa8c5c861d039b7dec6e81d72b4de44
Manju Sj <manjusj@Manjus-Mac.local>
2016-05-12 12:13:03 +0530

    Initial Commit

---
+++ IAdIntigration/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IAdIntigration
+//
+//  Created by Manju Sj on 5/12/16.
+//  Copyright (c) 2016 Manju Sj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IAdIntigration/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IAdIntigration
+//
+//  Created by Manju Sj on 5/12/16.
+//  Copyright (c) 2016 Manju Sj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
